a084254a4f302befed406373ae7536736d16788d
Markus Väänänen <markus.vaananen@gmail.com>
2020-09-17 14:53:25 +0300

    Chapter3 ex1

--- src/main/java/com/example/bookstore/domain/Book.java
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -1,70 +1,84 @@
 package com.example.bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
-	private String title;
-	private String author;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String title, author, isbn;
 	private int year;
-	private String isbn;
 	private long price;
 	
+	public Book() {}
 
 	public Book(String title, String author, int year, String isbn, long price) {
-		//super();
+		super();
+		//this.id = id;
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
 	}
+	
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getTitle() {
 		return title;
 	}
 
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 
 	public String getAuthor() {
 		return author;
 	}
 
-
 	public void setAuthor(String author) {
 		this.author = author;
 	}
-
 
 	public int getYear() {
 		return year;
 	}
 
-
 	public void setYear(int year) {
 		this.year = year;
 	}
-
 
 	public String getIsbn() {
 		return isbn;
 	}
 
-
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
-
 
 	public long getPrice() {
 		return price;
 	}
 
-
 	public void setPrice(long price) {
 		this.price = price;
+	}
+	
+	@Override
+	public String toString() { 
+		return " Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price +"]";
 	}
 
 }
--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,16 +1,31 @@
 package com.example.bookstore.web;
 
+import java.awt.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
+
+
+
 @Controller
 public class BookController {
-    @RequestMapping("/index") 
-    public String greeting(Model model) {
-        //model.addAttribute("name", name);
-        //model.addAttribute("age", age);
-        return "index";
+	
+    @Autowired
+    private BookRepository repository;
+    
+    public String save(Book book){
+        repository.save(book);
+        //return "redirect:studentlist";
+        return "test";
+    }    
+    
+    public void doSomething() {
+        //List < Book > books = repository.findByTitle("Edwards");
     }
 }
